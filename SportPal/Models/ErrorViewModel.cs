742cc4aa6176bd4c2618c31283e21f2b5f416390
JackDouma <jackdouma0@gmail.com>
2022-09-15 19:31:47 -0400

    Add project files.

---
+++ SportPal/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SportPal.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
