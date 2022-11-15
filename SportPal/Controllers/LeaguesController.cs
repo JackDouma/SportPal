93cc40afe0b29c101c9ad9085e6856dc8f060a25
JackDouma <jackdouma0@gmail.com>
2022-11-15 17:26:40 -0500

    made add, edit, and delete for both tables private and invisible for a user not logged in

--- SportPal/Controllers/LeaguesController.cs
+++ SportPal/Controllers/LeaguesController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -10,6 +11,7 @@ using SportPal.Models;
 
 namespace SportPal.Controllers
 {
+    [Authorize]
     public class LeaguesController : Controller
     {
         private readonly ApplicationDbContext _context;
@@ -18,13 +20,13 @@ namespace SportPal.Controllers
         {
             _context = context;
         }
-
+        [AllowAnonymous]
         // GET: Leagues
         public async Task<IActionResult> Index()
         {
               return View(await _context.Leagues.ToListAsync());
         }
-
+        [AllowAnonymous]
         // GET: Leagues/Details/5
         public async Task<IActionResult> Details(int? id)
         {
--- SportPal/Controllers/StandingsController.cs
+++ SportPal/Controllers/StandingsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -10,6 +11,7 @@ using SportPal.Models;
 
 namespace SportPal.Controllers
 {
+    [Authorize]
     public class StandingsController : Controller
     {
         private readonly ApplicationDbContext _context;
@@ -18,14 +20,14 @@ namespace SportPal.Controllers
         {
             _context = context;
         }
-
+        [AllowAnonymous]
         // GET: Standings
         public async Task<IActionResult> Index()
         {
             var applicationDbContext = _context.Standing.Include(s => s.League);
             return View(await applicationDbContext.OrderByDescending(s => s.Points).ToListAsync());
         }
-
+        [AllowAnonymous]
         // GET: Standings/Details/5
         public async Task<IActionResult> Details(int? id)
         {
